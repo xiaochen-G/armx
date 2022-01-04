93ae8b5b02bb1d5c79e2fe14eea8baf6f53f6bfa
晓晨 <93259271+xiaochen-G@users.noreply.github.com>
2022-01-04 15:22:41 +0800

    Add files via upload

---
+++ ext/compatibility/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+		<meta name="renderer" content="webkit">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta name="applicable-device" content="pc,mobile">
+		<meta name="MobileOptimized" content="width">
+		<meta name="HandheldFriendly" content="true">
+		<meta name="referrer" content="always">
+		<meta name="format-detection" content="telephone=no">
+		<meta name="theme-color" content="#FFFFFF" />
+		<title>兼容提示页</title>
+		<meta name="description" itemprop="description" content="Is there something better you could be doing with your time right now ? Let's start learning ! Here are a lot of interesting thing I wish to share with you." />
+		<meta name="keywords" content="vircloud,vircloudnet,virtualcloud,vircloudcom,viacloud,owenss,owenss's blog,vircloud" />
+		<link rel="shortcut icon" href="/usr/themes/armx/img/favicon.ico">
+		<link rel="Bookmark" href="/usr/themes/armx/img/favicon.ico">
+		<link rel="alternate"  href="https://vircloud.net/" hreflang="zh-Hans" />
+		<link rel="canonical"  href="https://vircloud.net/" />
+		<link rel="apple-touch-icon" href="/usr/themes/armx/img/apple-touch-icon.png">
+		<link rel="apple-touch-icon" sizes="57x57" href="/usr/themes/armx/img/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="/usr/themes/armx/img/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="/usr/themes/armx/img/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/usr/themes/armx/img/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="/usr/themes/armx/img/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="/usr/themes/armx/img/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="/usr/themes/armx/img/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="/usr/themes/armx/img/apple-touch-icon-180x180.png">
+		<style>body{margin:0;font-family:'Helvetica Neue',Helvetica,'PingFang SC','Hiragino Sans GB','Microsoft YaHei',Arial,sans-serif;font-size:15px;color:#404040}.Header{overflow:hidden;background-color:#fff;box-shadow:0 3px 4px rgba(0,0,0,.06)}.Header-inner{width:100%;margin:0 auto;padding:0 16px}.Header-logo{width:50px;height:50px;margin:5px 0}.Header-logo img{display:block;width:100%;height:100%}.Compatibility{width:100%;margin:0 auto}.Compatibility-icon{width:112px;height:83px;margin:64px auto}.Compatibility-icon img{display:block;width:100%;height:100%}.Compatibility-tip{font-size:24px;font-weight:700;text-align:center;margin:0 auto;width:80%}.Anyway{display:none}.CompatibilityBrowsers{overflow:hidden;margin-top:64px}.CompatibilityBrowsers-item{position:relative;float:left;width:50%;padding:16px 0}a.CompatibilityBrowsers-item{text-decoration:none;color:#3e7ac2}.CompatibilityBrowsers-divider{position:absolute;right:0;top:24px;height:64px;border-right:1px solid #ebeef5}.CompatibilityBrowsers-icon{width:80px;height:80px;margin:0 auto}.CompatibilityBrowsers-icon img{display:block;border-style:none;}.CompatibilityBrowsers-ieIcon img{width:88px;height:81px;margin-left:-8px;margin-top:-1px}.CompatibilityBrowsers-chromeIcon img{width:83px;height:83px;margin:-1.5px}.CompatibilityBrowsers-text{margin-top:32px;text-align:center}.button666{display:inline-block;border-radius:4px;background-color:#38b4da;border:none;color:#FFF;text-align:center;font-size:18px;padding:15px;min-width:175px;transition:all .5s;cursor:pointer;margin:5px}.button666{box-shadow:0 8px 16px 0 rgba(0,0,0,.2),0 6px 20px 0 rgba(0,0,0,.19)}.button666 span{cursor:pointer;display:inline-block;position:relative;transition:.5s}</style>
+	</head>
+	<body>
+    <div class="Header">
+      <div class="Header-inner">
+        <div class="Header-logo">
+          <img src="images/logo@2x.png" srcset="images/logo.png 1x, images/logo@2x.png 2x, images/logo@3x.png 3x" />
+        </div>
+      </div>
+    </div>
+	<div class="Compatibility">
+		<div class="Compatibility-icon">
+			<img src="images/icon@2x.png" srcset="images/icon.png 1x, images/icon@2x.png 2x, images/icon@3x.png 3x" />
+		</div>
+		<div class="Compatibility-tip">您正在使用的浏览器版本过低，不能正常访问本站，建议：</div>
+		<div class="CompatibilityBrowsers">
+			<a href="https://www.microsoft.com/zh-cn/download/internet-explorer.aspx" class="CompatibilityBrowsers-item">
+				<div class="CompatibilityBrowsers-icon CompatibilityBrowsers-ieIcon">
+					<img src="images/internet-explorer@2x.png" srcset="images/internet-explorer.png 1x, images/internet-explorer@2x.png 2x, images/internet-explorer@3x.png 3x" />
+				</div>
+				<div class="CompatibilityBrowsers-text">升级 Internet Explorer</div>
+				<div class="CompatibilityBrowsers-divider"></div>
+			</a>
+			<a href="https://www.google.com/chrome/" class="CompatibilityBrowsers-item">
+				<div class="CompatibilityBrowsers-icon CompatibilityBrowsers-chromeIcon">
+					<img src="images/google-chrome@2x.png" srcset="images/google-chrome.png 1x, images/google-chrome@2x.png 2x, images/google-chrome@3x.png 3x" />
+				</div>
+				<div class="CompatibilityBrowsers-text ">使用 Google Chrome</div>
+			</a>
+		</div>
+		<div class="Compatibility-tip Anyway"><br><br>
+			<button class="button666" onclick="{location.href='/'}">
+				<span id="continue">没关系，我要继续</span>
+			</button>
+		</div>
+	</div>
+	</body>
+</html>
